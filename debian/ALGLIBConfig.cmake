545b85224af9e6460e75d07c723aca6307f66590
Deepin Developer <deepin-dev@deepin.org>
2022-09-28 10:17:00 +0800

    feat: Init commit
    init alglib
    
    Log:

---
+++ debian/ALGLIBConfig.cmake
@@ -0,0 +1,16 @@
+# - Try to find alglib
+# Once done this will define
+#  ALGLIB_INCLUDE_DIRS - The alglib include directory
+#  ALGLIB_LIB - The libraries needed to use alglib
+#  ALGLIB_FOUND -  True if ALGLIB found.
+
+set(ALGLIB_FOUND TRUE)
+
+find_path (ALGLIB_INCLUDE_DIRS 
+  ap.h
+  PATHS ${CMAKE_INSTALL_PREFIX}/include
+  PATH_SUFFIXES libalglib)
+
+find_library (ALGLIB_LIB NAMES alglib)
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (ALGLIB DEFAULT_MSG ALGLIB_LIB ALGLIB_INCLUDE_DIRS)
